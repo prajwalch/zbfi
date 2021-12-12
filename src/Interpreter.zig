64e96787fd22ba7ec394b3b9240907734d7c90b3
PrajwalCH <prajjwal2058@gmail.com>
2021-12-12 21:56:23 +0545

    feat: add findEndOfLoop() function and fix/refactor other stuffs
    * Remove Loop struct and use only usize for loop stack
      since we are using ony one field (start_index) to store index of loop
      start there is no need to use struct for that
    
    * Find loop end and start index in startLoop() and endLoop() function
      before pushing the new loop on to the loop_stack be sure we have
      matching '[' which same goes for endLoop() but find matching ']'
    
    * Improve comment on endLoop()

--- src/Interpreter.zig
+++ src/Interpreter.zig
@@ -7,17 +7,13 @@ const stdout = std.io.getStdOut().writer();
 
 const MEMORY_SIZE = 30_000;
 
-const Loop = struct {
-    start_index: usize,
-};
-
 src: []const u8,
 src_current_index: usize = 0,
 src_next_index: usize = 0,
 
 mem: [MEMORY_SIZE]u8 = [_]u8{0} ** MEMORY_SIZE,
 mem_index: usize = 0,
-loop_stack: std.ArrayList(Loop) = std.ArrayList(Loop).init(allocator),
+loop_stack: std.ArrayList(usize) = std.ArrayList(usize).init(allocator),
 
 pub fn init(src: []const u8) Self {
     return Self{ .src = src };
@@ -52,24 +48,30 @@ pub fn decreaseValue(self: *Self) void {
 }
 
 pub fn startLoop(self: *Self) !void {
+    const loop_end_index = self.findEndOfLoop() orelse {
+        std.debug.print("syntax error: found unmatched ']' of index {d}\n ", .{self.src_current_index});
+        std.process.exit(1);
+    };
+
     if (self.mem[self.mem_index] == 0) {
         // jump to matching ']' (end of loop)
+        self.src_next_index = loop_end_index + 1;
     } else {
-        try self.loop_stack.append(Loop{ .start_index = self.src_current_index });
+        try self.loop_stack.append(self.src_current_index);
     }
 }
 
 pub fn endLoop(self: *Self) void {
-    const last_loop_stack = self.loop_stack.popOrNull() orelse {
-        std.debug.print("found loop ] without matching [ at index {d}\n", .{self.src_current_index});
+    const loop_start_index = self.loop_stack.popOrNull() orelse {
+        std.debug.print("syntax error: found unmatched '[' of index {d}\n", .{self.src_current_index});
         std.process.exit(1);
     };
 
-    // if current value is 0 it will automatically jump
-    // to next instruction using 'src_next_index'
+    // Here no need to check wether current value is zero or not
+    // because it will jump to next instruction anyway using 'src_next_index'
     if (self.mem[self.mem_index] != 0) {
         // jump to matching '[' (start of loop)
-        self.src_next_index = last_loop_stack.start_index;
+        self.src_next_index = loop_start_index;
     }
 }
 
@@ -81,4 +83,25 @@ pub fn readChar(self: *Self) !void {
 
 pub fn writeChar(self: *Self) !void {
     try stdout.writeByte(self.mem[self.mem_index]);
+}
+
+fn findEndOfLoop(self: *Self) ?usize {
+    var nums_open_bracket: usize = 0;
+    var nums_close_bracket: usize = 0;
+
+    var src_slice = self.src[self.src_current_index..];
+    for (src_slice) |c, i| {
+        if (c == '[') {
+            nums_open_bracket += 1;
+        } else if (c == ']') {
+            nums_close_bracket += 1;
+        }
+
+        if (nums_open_bracket > 0) {
+            if (nums_open_bracket == nums_close_bracket) {
+                return self.src_current_index - 1;
+            }
+        }
+    }
+    return null;
 }
