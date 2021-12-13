c977a94b8c337efc6479c06767b50580a1a03edf
PrajwalCH <prajjwal2058@gmail.com>
2021-12-13 11:46:54 +0545

    refactor: make allocator as const global and use it on other places

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,14 @@
 const std = @import("std");
 const Interpreter = @import("Interpreter.zig");
 
+const allocator = std.heap.page_allocator;
+
 pub fn main() anyerror!void {
     try interpret("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.");
 }
 
 fn interpret(src: []const u8) !void {
-    var interpreter = Interpreter.init(std.heap.page_allocator, src);
+    var interpreter = Interpreter.init(allocator, src);
     errdefer interpreter.deinit();
 
     while (interpreter.next()) |operator| {
