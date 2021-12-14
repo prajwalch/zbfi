85c9a33ef857cb8223698beaf6f2ccd4dc628765
PrajwalCH <prajjwal2058@gmail.com>
2021-12-14 19:58:36 +0545

    refactor: check src_next_index first before processing next

--- src/Interpreter.zig
+++ src/Interpreter.zig
@@ -78,8 +78,9 @@ pub fn deinit(self: Self) void {
 }
 
 pub fn next_command(self: *Self) ?u8 {
+    if (self.src_next_index >= self.src.len) return null;
+
     self.src_current_index = self.src_next_index;
-    if (self.src_current_index >= self.src.len) return null;
     self.src_next_index += 1;
     return self.src[self.src_current_index];
 }
