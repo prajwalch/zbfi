3aa602b5bdeb747c05ca9adab5879caae714ed4b
PrajwalCH <prajjwal2058@gmail.com>
2021-12-18 20:35:51 +0545

    feat: add runInteractiveMode() function

--- src/main.zig
+++ src/main.zig
@@ -17,38 +17,7 @@ pub fn main() anyerror!void {
     if (zbfi_args.is_help) {
         return;
     }
-
-    if (zbfi_args.src_file) |file_path| {
-        var content = readSrcFile(file_path) catch |err| switch (err) {
-            error.InvalidFileFormat => {
-                std.debug.warn("Error: Invalid File Format\n", .{});
-                std.process.exit(1);
-            },
-            error.FileNotFound => {
-                std.debug.warn("Error: File not found\n", .{});
-                std.process.exit(1);
-            },
-            else => |e| return e,
-        };
-        defer allocator.free(content);
-
-        _ = Interpreter.interpret(allocator, content);
-        return;
-    }
-
-    while (true) {
-        std.debug.print("> ", .{});
-
-        var src_input = try utils.stdin.readUntilDelimiterOrEofAlloc(allocator, '\n', 1024);
-        if (src_input) |src| {
-            if (std.mem.eql(u8, src, ";quit")) {
-                break;
-            }
-            _ = Interpreter.interpret(allocator, src);
-            std.debug.print("\n", .{});
-        }
-    }
-    std.debug.print("[Interpreter Ended]\n", .{});
+    try runInteractiveMode();
 }
 
 fn readSrcFile(file_path: []const u8) ![]u8 {
@@ -66,4 +35,20 @@ fn isValidExtension(file_path: []const u8) bool {
     if (!std.mem.eql(u8, file_ext, ".b") and !std.mem.eql(u8, file_ext, ".bf"))
         return false;
     return true;
+}
+
+fn runInteractiveMode() !void {
+    while (true) {
+        std.debug.print("> ", .{});
+
+        var src_input = try utils.stdin.readUntilDelimiterOrEofAlloc(allocator, '\n', 1024);
+        if (src_input) |src| {
+            if (std.mem.eql(u8, src, ";quit")) {
+                break;
+            }
+            _ = Interpreter.interpret(allocator, src);
+            std.debug.print("\n", .{});
+        }
+    }
+    std.debug.print("[Interpreter Ended]\n", .{});
 }
